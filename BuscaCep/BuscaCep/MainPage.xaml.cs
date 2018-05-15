61f87e534a3fc48b2d088e3b0fe13d51cfd9a0f2
Ronye do Lago <ronye.rocha@gmail.com>
2018-05-15 13:43:27 -0300

    teste viaCep

--- BuscaCep/BuscaCep/MainPage.xaml.cs
+++ BuscaCep/BuscaCep/MainPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
@@ -13,5 +14,38 @@ namespace BuscaCep
 		{
 			InitializeComponent();
 		}
-	}
+
+        private async void BtnBuscarCep_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                if (string.IsNullOrWhiteSpace(txtCep.Text))
+                {
+                    throw new InvalidOperationException("Digite um CEP válido para a busca.");
+                }
+
+                using (var client = new HttpClient())
+                {
+                    using (var response = await client.GetAsync($"http://viacep.com.br/ws/{txtCep.Text}/json/"))
+                    {
+                        if (!response.IsSuccessStatusCode)
+                        {
+                            throw new InvalidOperationException("Algo de errado não deu certo na sua consulta do CEP.");
+                        }
+
+                        var result = await response.Content.ReadAsStringAsync();
+
+                        if (!string.IsNullOrWhiteSpace(result))
+                        {
+                            await DisplayAlert("Eitha!", result, "Ok");
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Deu ruim :-(", ex.Message, "Ok");
+            }
+        }
+    }
 }
