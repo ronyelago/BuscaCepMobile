469227c52a144194e86ad4fd84d3075da73823ed
Ronye do Lago <ronye.rocha@gmail.com>
2018-05-15 15:04:15 -0300

    organizando tudo

---
+++ BuscaCep/BuscaCep/Clients/ViaCepHttpClient.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace BuscaCep.Clients
+{
+    public class ViaCepHttpClient
+    {
+        private static Lazy<ViaCepHttpClient> _lazy = new Lazy<ViaCepHttpClient>(() => new ViaCepHttpClient());
+        private readonly HttpClient _httpClient;    
+
+        public static ViaCepHttpClient Current { get => _lazy.Value; }
+
+        private ViaCepHttpClient()
+        {
+            _httpClient = new HttpClient();
+        }
+
+        public async Task<string> BuscarCep(string cep)
+        {
+            try
+            {
+                if (string.IsNullOrWhiteSpace(cep))
+                {
+                    throw new InvalidOperationException("Digite um CEP válido para a busca.");
+                }
+
+
+                using (var response = await _httpClient.GetAsync($"http://viacep.com.br/ws/{cep}/json/"))
+                {
+                    if (!response.IsSuccessStatusCode)
+                    {
+                        throw new InvalidOperationException("Algo de errado não deu certo na sua consulta do CEP.");
+                    }
+
+                    return await response.Content.ReadAsStringAsync();
+                }
+                
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
--- BuscaCep/BuscaCep/MainPage.xaml.cs
+++ BuscaCep/BuscaCep/MainPage.xaml.cs
@@ -1,14 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using BuscaCep.Clients;
+using System;
 using System.Net.Http;
-using System.Text;
-using System.Threading.Tasks;
 using Xamarin.Forms;
 
 namespace BuscaCep
 {
-	public partial class MainPage : ContentPage
+    public partial class MainPage : ContentPage
 	{
 		public MainPage()
 		{
@@ -19,32 +16,17 @@ namespace BuscaCep
         {
             try
             {
-                if (string.IsNullOrWhiteSpace(txtCep.Text))
+                var result = await ViaCepHttpClient.Current.BuscarCep(txtCep.Text);
+
+                if (!string.IsNullOrWhiteSpace(result))
                 {
-                    throw new InvalidOperationException("Digite um CEP válido para a busca.");
-                }
-
-                using (var client = new HttpClient())
-                {
-                    using (var response = await client.GetAsync($"http://viacep.com.br/ws/{txtCep.Text}/json/"))
-                    {
-                        if (!response.IsSuccessStatusCode)
-                        {
-                            throw new InvalidOperationException("Algo de errado não deu certo na sua consulta do CEP.");
-                        }
-
-                        var result = await response.Content.ReadAsStringAsync();
-
-                        if (!string.IsNullOrWhiteSpace(result))
-                        {
-                            await DisplayAlert("Eitha!", result, "Ok");
-                        }
-                    }
+                    await DisplayAlert("Eitha!", result, "Ok");
                 }
             }
-            catch (Exception ex)
+            catch (Exception)
             {
-                await DisplayAlert("Deu ruim :-(", ex.Message, "Ok");
+
+                throw;
             }
         }
     }
